7eb2460abc7888c62b569ad948647dc407bb1ced
EmbeddedAT <Peter@EmbeddedAT.com>
2022-03-23 19:46:55 +0100

    Refactoring the INT_Marshal

--- src/main.cpp
+++ src/main.cpp
@@ -34,12 +34,14 @@
 #include "batterymonitor.h"
 #include "UI\UI.h"
 #include "ICM_20948.h" 
-#include <Centipede.h>
+#include <INT_Marshal.h>
 
 
 
 SensorFactory sensors {};
 int sensorToCalibrate = -1;
+uint16_t IMU_INT_Index;
+bool IMU_INT_Event = false;
 bool blinking = false;
 unsigned long blinkStart = 0;
 unsigned long loopTime = 0;
@@ -47,10 +49,41 @@ unsigned long last_rssi_sample = 0;
 bool secondImuActive = false;
 BatteryMonitor battery;
 
+INT_Marshal CS; 
+
+#define INTPIN 4
+#define controlArduioInt attachInterrupt(digitalPinToInterrupt(INTPIN),isr,FALLING)
+
+void isr(void)
+{
+   noInterrupts();
+   detachInterrupt(digitalPinToInterrupt(INTPIN));
+   interrupts(); 
+   IMU_INT_Event = true;
+   IMU_INT_Index =  CS.getIntF(0);
+   CS.portCaptureRead(0);
+   controlArduioInt;  
+}
+
+
+
 void setup()
 {
 
 pinMode(MUX_Reset_Pin, INPUT);
+ 
+CS.initialize();
+
+CS.portMode(0, 0xffff);   // Chip pins on port to inputs.
+CS.portPullup(0, 0xffff); // All pins on port to pullup.
+CS.portInterrupts(0, 0x0000, 0xFFFF, 0X0000);  //
+CS.portCaptureRead(0); // Read capture reg. to clear ints.
+CS.portInterrupts(1, 0xffff, 0xFFFF, 0X0000);
+CS.portIntPinConfig(0,1,0);
+  
+
+  controlArduioInt; // Enable Arduino interrupt control.
+
 
 UI::Setup();
 UI::DrawSplash();
